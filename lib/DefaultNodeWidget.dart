91eb834c452700d39aae4eaacfc6d78bb9c82c71
Krishna Subramanian <krishna.subramanian@devolo.de>
2023-07-24 11:54:11 +0200

    Expand default node widget to include icon, names, and clients

--- lib/DefaultNodeWidget.dart
+++ lib/DefaultNodeWidget.dart
@@ -1,16 +1,84 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'NetworkGraphViewWrapper.dart';
 
-Widget DefaultNodeWidget(int? nodeId) {
-  return Container(
-      padding: EdgeInsets.all(32.0),
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        shape: BoxShape.circle,
+Widget DefaultNodeWidget(int? nodeId, DetailLevel detailLevel) {
+  return Stack(
+    clipBehavior: Clip.none,
+    children: [
+      Container(padding: EdgeInsets.all(32.0),
+        decoration: BoxDecoration(
+          color: Color(0xFF0072B4), // devolo blue
+          shape: BoxShape.circle,
+        ),
+        child: Icon(
+          Icons.rectangle_outlined,
+          size: 32,
+          color: Colors.white,
+        )
       ),
-      child: Text(
-        'Node ${nodeId}',
-        style: TextStyle(color: Colors.black45),
-      )
+      if (detailLevel != DetailLevel.Low)...[
+        Positioned(
+          bottom: -20,
+          right: 10,
+          child: Container(
+            color: Colors.white,
+            child: Text(
+              'Device name',
+              style: TextStyle(color: detailLevel == DetailLevel.Low ? Colors.transparent : Colors.black87, fontSize: 12.0),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: -40,
+          right: 10,
+          child: Container(
+            color: Colors.white,
+            child: Text(
+              'Product type',
+              style: TextStyle(color: detailLevel == DetailLevel.Low ? Colors.transparent : Colors.black87, fontSize: 12.0),
+            ),
+          ),
+        )
+      ],
+      if (detailLevel == DetailLevel.High)...[
+        Positioned(
+          top: 0,
+          right: 5,
+          child: Container(
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.green,),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 5,
+          child: Container(
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.green,),
+          ),
+        ),
+        Positioned(
+          top: 35,
+          left: -15,
+          child: Container(
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.green,),
+          ),
+        ),
+        Positioned(
+          top: 35,
+          right: -15,
+          child: Container(
+            width: 10,
+            height: 10,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.green,),
+          ),
+        ),
+      ]
+    ],
   );
 }
